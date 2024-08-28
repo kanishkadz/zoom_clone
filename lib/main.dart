23495c8e9934a717c927eb19b65c03c961036a5b
Kanishka Anand <kanishkaadz@gmail.com>
2024-08-28 21:53:29 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
       home: const Text("Hello"),
     );
