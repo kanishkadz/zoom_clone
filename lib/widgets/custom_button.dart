5fca84e7917fd6ddd3a047e949db6339025acb98
Kanishka Anand <kanishkaadz@gmail.com>
2024-09-05 21:55:11 +0530

    Update custom_button.dart

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom/utils/colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
@@ -9,6 +10,10 @@ class CustomButton extends StatelessWidget {
     return ElevatedButton(
         onPressed: (){},
         child: Text(text, style: TextStyle(fontSize: 17,)),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+          minimumSize: const Size(double.infinity, 50),
+        ),
     );
   }
 }
